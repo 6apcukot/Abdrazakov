5733bb2ba416dd993bc6c221fe8a7d959ae9fef4
Arlsan <minearsyt@gmail.com>
2022-03-16 12:09:15 +0300

    new

--- 4-3-pwm.py
+++ 4-3-pwm.py
@@ -3,6 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BCM)   
 pin = 22
+
 GPIO.setup(pin, GPIO.OUT)
 
 p = GPIO.PWM(pin, 1000)
@@ -13,12 +14,12 @@ p = GPIO.PWM(pin, 1000)
 try:
     while(1):
         s = float(input("Скважность: "))
-        i
         if s == 0:
             s = 1
         p.start(1/s*100)
         
         if input("Press q to stop: ") == 'q':
+            p.stop()
             break
 finally:
     GPIO.output(pin, 0)
